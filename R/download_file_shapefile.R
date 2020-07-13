073dca08caba333443fabd78a5a618218b6536f9
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-13 12:38:22 -0300

    Add files

--- R/download_file_shapefile.R
+++ R/download_file_shapefile.R
@@ -17,11 +17,12 @@ download_file_shapefile <- function(index){
   i <- index
   require(sf)
   shape <- switch (i,
-    read_sf(url("https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/IMOVEIS_PUBLICOS_RURAIS.shp")),
-    "https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/AREA_DE_RISCO.shp",
-    "https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/REDE_DRENAGEM.shp",
-    "https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/MACROZONAS.shp",
-    "https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/LOTEAMENTOS.shp"
+    read_sf(dsn="https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/")
+    ,layer="IMOVEIS_PUBLICOS_RURAIS"),
+    #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/AREA_DE_RISCO.shp",
+    #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/REDE_DRENAGEM.shp",
+    #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/MACROZONAS.shp",
+    #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/LOTEAMENTOS.shp"
   )
   return(shape)
 }
