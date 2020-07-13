830d63b7fda09bdb3f7ea65501ca7d0ccd7076e3
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-13 10:17:48 -0300

    Add files

--- R/download_file_shapefile.R
+++ R/download_file_shapefile.R
@@ -1,3 +1,18 @@
+#' Download file shapefile
+#'
+#' Function return files format shapefile
+#'
+#' @export
+#' @family general area functions
+#' @param index Variable define type of shapefile
+#' @examples \dontrun{
+#'
+#' library(geobage)
+#'
+#'   c <- download_file_shapefile()
+#'
+#' }
+
 download_file_shapefile <- function(index){
   i <- index
   if(i == 1){
