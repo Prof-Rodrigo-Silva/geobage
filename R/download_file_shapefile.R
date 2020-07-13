474a48ace559a254c5e598528f1f7b0e506602a3
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-13 12:41:45 -0300

    Add files

--- R/download_file_shapefile.R
+++ R/download_file_shapefile.R
@@ -17,8 +17,8 @@ download_file_shapefile <- function(index){
   i <- index
   require(sf)
   shape <- switch (i,
-    read_sf(dsn="https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/")
-    ,layer="IMOVEIS_PUBLICOS_RURAIS")
+    read_sf(dsn="https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/"
+            ,layer="IMOVEIS_PUBLICOS_RURAIS")
     #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/AREA_DE_RISCO.shp",
     #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/REDE_DRENAGEM.shp",
     #"https://github.com/Prof-Rodrigo-Silva/arquivos_geobage/blob/master/MACROZONAS.shp",
