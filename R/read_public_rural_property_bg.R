3c1eafacd0ee562272fd3016b6129f296ef5e7c9
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-13 09:16:02 -0300

    Add files

---
+++ R/read_public_rural_property_bg.R
@@ -0,0 +1,17 @@
+#' Public rural property
+#'
+#' Function returns the Public rural property inserted in municipality of BagÃ©
+#'
+#' @export
+#' @family general area functions
+#' @examples \dontrun{
+#'
+#' library(geobage)
+#'
+#'   c <- read_public_rural_property_bg()
+#'
+#' }
+read_public_rural_property_bg <- function(){
+  #bg <- data(imoveisRuraisPublicos)
+  #bg
+}
