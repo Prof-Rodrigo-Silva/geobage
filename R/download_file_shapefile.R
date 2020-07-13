163e37dcb8e5bad1f8dd22303d8f2fa892c66f6a
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-13 11:10:21 -0300

    Add files

--- R/download_file_shapefile.R
+++ R/download_file_shapefile.R
@@ -16,10 +16,10 @@
 download_file_shapefile <- function(index){
   i <- index
   switch (i,
-    1 <- {return("Em breve: Imóveis Públicos Rurais")},
-    2 <- {return("Em breve: Área de Risco")},
-    3 <- {return("Em breve: Rede de Drenagem")},
-    4 <- {return("Em breve: Macrozonas")},
-    5 <- {return("Em breve: Loteamentos")}
+    "Em breve: Imóveis Públicos Rurais",
+    "Em breve: Área de Risco",
+    "Em breve: Rede de Drenagem",
+    "Em breve: Macrozonas",
+    "Em breve: Loteamentos"
   )
 }
